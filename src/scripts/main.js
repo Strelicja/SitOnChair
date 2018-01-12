d8dec798ab8b733cdef945823725abd29c9097e8
Iwona Janiak <iwona.janiak@havas.com>
2018-01-12 12:49:09 +0100

    add touchstart in moblie sl slider

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,11 +2,13 @@
 document.addEventListener("DOMContentLoaded", function() {
     var arrow_left = document.querySelector('.arrow_left');
     var arrow_right = document.querySelector('.arrow_right');
-    var chairs_img = document.querySelectorAll('.slider_img ul li');
+    var chairs_img = document.querySelectorAll('.slider_img ul li'); 
+    var chairs_img_touch = document.querySelectorAll('.slider_img ul li img'); 
 
     var index = 0;
 
     chairs_img[index].style.display = "initial";
+//SLIDER ARROWS - CLICK//
 
     arrow_left.addEventListener("click", function() {
         chairs_img[index].style.display = "none";
@@ -26,4 +28,25 @@ document.addEventListener("DOMContentLoaded", function() {
         chairs_img[index].style.display = "initial";
     });
 
-});
+//SLIDER MOBILE -TOUCHSTART//
+    chairs_img_touch.forEach(function (item) {
+        item.addEventListener('touchstart', function() {
+            chairs_img[index].style.display = "none";
+        index--;
+        if (index < 0) {
+            index = chairs_img.length - 1;
+        }
+        chairs_img[index].style.display = "initial";
+        console.log('sdf')
+        })
+    })
+
+    
+
+
+
+
+
+    });
+
+
